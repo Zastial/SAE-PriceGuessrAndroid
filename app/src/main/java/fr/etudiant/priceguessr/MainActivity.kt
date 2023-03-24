9e4cbce72b977477d5de0b018153ebb9740db823
mathis <mathis.michenaud@etu.univ-nantes.fr>
2023-03-24 16:22:01 +0100

    game fragment load in home page and first design

--- app/src/main/java/fr/etudiant/priceguessr/MainActivity.kt
+++ app/src/main/java/fr/etudiant/priceguessr/MainActivity.kt
@@ -2,10 +2,22 @@ package fr.etudiant.priceguessr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans la boite (fragment container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, GameFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun loadFrangment(fragment: Fragment) {
+        //val transaction = supportFragmentManager.beginTransaction()
+        //transaction.replace(R.id.fragment_container, GameFragment)
     }
 }
