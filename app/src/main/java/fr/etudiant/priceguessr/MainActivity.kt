5b6f838fd98247b93544c64e49e73e5b8869d0dd
MathisMchd <michenaudmathis@gmail.com>
2023-03-25 17:25:54 +0100

    profile setting UI and navigation logic (fragment) started

--- app/src/main/java/fr/etudiant/priceguessr/MainActivity.kt
+++ app/src/main/java/fr/etudiant/priceguessr/MainActivity.kt
@@ -2,9 +2,12 @@ package fr.etudiant.priceguessr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import fr.etudiant.priceguessr.fragments.GameFragment
 import fr.etudiant.priceguessr.fragments.HistoryFragment
+import fr.etudiant.priceguessr.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +23,39 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        // import navigation view
+        /*
+        val navigationView = findViewById<NavigationView>(R.id.bottomNavigationView)
+        navigationView.setNavigationItemSelectedListener {
 
+            when(it.itemId)
+            {
+                R.id.home -> {
+                    // il faut charger le fragment
+                    loadFrangment(GameFragment())
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.history -> {
+                    // il faut charger le fragment
+                    loadFrangment(HistoryFragment())
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.profile-> {
+                    // il faut charger le fragment
+                    loadFrangment(ProfileFragment())
+                    return@setNavigationItemSelectedListener true
+                }
+                else -> {
+                    Log.e("TAG", "false")
+                    false
+                }
+            }
+        }*/
 
 
         //injecter le fragment dans la boite (fragment container)
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, HistoryFragment())
+        transaction.replace(R.id.fragment_container, ProfileFragment())
         transaction.addToBackStack(null)
         transaction.commit()
     }
--- app/src/main/java/fr/etudiant/priceguessr/fragments/GameFragment.kt
+++ app/src/main/java/fr/etudiant/priceguessr/fragments/GameFragment.kt
@@ -10,11 +10,7 @@ import fr.etudiant.priceguessr.R
 
 class GameFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_game, container, false)
     }
 }
