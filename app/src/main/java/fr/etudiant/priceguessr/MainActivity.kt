ec44a8e04c351501c83cf51ab5a1205113689942
E211769J <mathis.michenaud@etu.univ-nantes.fr>
2023-03-27 11:13:23 +0200

    navigation bar fix

--- app/src/main/java/fr/etudiant/priceguessr/MainActivity.kt
+++ app/src/main/java/fr/etudiant/priceguessr/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import fr.etudiant.priceguessr.fragments.GameFragment
 import fr.etudiant.priceguessr.fragments.HistoryFragment
@@ -22,42 +23,39 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
-        // import navigation view
-        /*
-        val navigationView = findViewById<NavigationView>(R.id.bottomNavigationView)
-        navigationView.setNavigationItemSelectedListener {
+        //first load GameFragment()
+        loadFragment(GameFragment())
+        // navigation View Logic
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navigationView.setOnItemSelectedListener {
 
             when(it.itemId)
             {
                 R.id.home -> {
-                    // il faut charger le fragment
-                    loadFrangment(GameFragment())
-                    return@setNavigationItemSelectedListener true
+                    loadFragment(GameFragment())
+                    true
                 }
                 R.id.history -> {
-                    // il faut charger le fragment
-                    loadFrangment(HistoryFragment())
-                    return@setNavigationItemSelectedListener true
+                    loadFragment(HistoryFragment())
+                    true
                 }
                 R.id.profile-> {
-                    // il faut charger le fragment
-                    loadFrangment(ProfileFragment())
-                    return@setNavigationItemSelectedListener true
+                    loadFragment(ProfileFragment())
+                    true
                 }
                 else -> {
                     Log.e("TAG", "false")
                     false
                 }
             }
-        }*/
+        }
 
 
         //injecter le fragment dans la boite (fragment container)
-        val transaction = supportFragmentManager.beginTransaction()
+       /* val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, ProfileFragment())
         transaction.addToBackStack(null)
-        transaction.commit()
+        transaction.commit()*/
     }
 
     // verify ..
@@ -67,8 +65,9 @@ class MainActivity : AppCompatActivity() {
         return isLogged
     }
 
-    private fun loadFrangment(fragment: Fragment) {
-        //val transaction = supportFragmentManager.beginTransaction()
-        //transaction.replace(R.id.fragment_container, GameFragment)
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
