40b0eedf0f56e22aa93192836ec6e7287aa775e6
MathisMchd <michenaudmathis@gmail.com>
2023-03-27 18:23:11 +0200

    fix localhost by 10.0.2.2

--- app/src/main/java/fr/etudiant/priceguessr/Constants.kt
+++ app/src/main/java/fr/etudiant/priceguessr/Constants.kt
@@ -3,7 +3,7 @@ package fr.etudiant.priceguessr
 class Constants {
 
     companion object {
-        val API_BASE_URl = "http://localhost:3000"
-        val API_GET_DALY_PRODUCT = "/product/daily"
+        val API_BASE_URl = "http://10.0.2.2:3000"
+        val API_GET_DALY_PRODUCT = "/product"
     }
 }
