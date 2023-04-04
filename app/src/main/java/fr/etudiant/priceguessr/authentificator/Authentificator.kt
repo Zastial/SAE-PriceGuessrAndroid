d8e51018650f874d1bee173fa02d8c246a120b18
Mathis MICHENAUD <E211769J@U-INF-J-F222-03.ad.iut-nantes.univ-nantes.prive>
2023-04-04 08:08:15 +0200

    delete authentificator class (don't use it)

--- app/src/main/java/fr/etudiant/priceguessr/authentificator/Authentificator.kt
+++
@@ -1,7 +0,0 @@
-package fr.etudiant.priceguessr.authentificator
-
-import android.accounts.AbstractAccountAuthenticator
-import android.content.Context
-
-class Authentificator(context: Context) {
-}
